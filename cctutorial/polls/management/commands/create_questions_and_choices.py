bdc2e3a64f5ec5a673fde47f6ea47c3932dc3b63
name <email>
2023-09-16 11:29:47 -0500

    ic/feat: add/upgrade templates and add command to generate data

--- cctutorial/polls/management/commands/create_questions_and_choices.py
+++ cctutorial/polls/management/commands/create_questions_and_choices.py
@@ -1,23 +1,27 @@
 from faker import Faker
-from django.core.management.base import BaseCommand, CommandError
-from polls.models import Question, Choice
+from django.core.management.base import BaseCommand, CommandError  # Add Error
+from ...models import Question, Choice
 
 
 class Command(BaseCommand):
-    help = 'Generate fake data for questions and choices'
+    help = "Generate fake data for questions and choices"
 
     def add_arguments(self, parser):
         parser.add_argument(
-            '--amount', default=10,
-            type=int, help='[Optional] Number of questions to create',
+            "--amount",
+            default=10,
+            type=int,
+            help="[Optional] Number of questions to create",
         )
 
     def handle(self, *args, **options):
         fake = Faker()
-        amount = options['amount']
+        amount = options["amount"]
         for _ in range(amount):
             question_text = fake.sentence()
-            pub_date = fake.date_time_between(start_date='-30d', end_date='now')
+            pub_date = fake.date_time_between(
+                start_date="-30d", end_date="now"
+            )
             question = Question.objects.create(
                 question_text=question_text,
                 pub_date=pub_date,
@@ -31,4 +35,8 @@ class Command(BaseCommand):
                     choice_text=choice_text,
                     votes=votes,
                 )
-        self.stdout.write(self.style.SUCCESS(f'Successfully created {amount} dummy questions and choices.'))
+        self.stdout.write(
+            self.style.SUCCESS(
+                f"Successfully created {amount} dummy questions and choices."
+            )
+        )
